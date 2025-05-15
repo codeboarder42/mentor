c81e15e6e74c53f1bd7c1c63ee1fa8c8e524f840
Fabien Quillévéré <quillevere.fabien@gmail.com>
2025-05-15 19:18:31 +0200

    test: add level.controller first test

--- src/level/level.controller.spec.ts
+++ src/level/level.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LevelController } from './level.controller';
+import { LevelService } from './level.service';
 
 describe('LevelController', () => {
   let controller: LevelController;
+  let service = { createNewLevel: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LevelController],
-    }).compile();
+      providers: [LevelService],
+    })
+      .overrideProvider(LevelService)
+      .useValue(service)
+      .compile();
 
     controller = module.get<LevelController>(LevelController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a new level', () => {
+    const spyLevelService = jest
+      .spyOn(service, 'createNewLevel')
+      .mockImplementation(() => ({
+        id: 1,
+        name: 'test-level',
+      }));
+    const result = controller.addLevel({ name: 'test-level' });
+    expect(spyLevelService).toHaveBeenCalledTimes(1);
+    expect(spyLevelService).toHaveBeenCalledWith({ name: 'test-level' });
+    expect(result).toStrictEqual({
+      id: 1,
+      name: 'test-level',
+    });
   });
 });
