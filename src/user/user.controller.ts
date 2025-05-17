2edd899a5f2525153bd8fd0b32ca77c158b1820f
Fabien Quillévéré <quillevere.fabien@gmail.com>
2025-05-17 10:16:16 +0200

    feat: add user creation endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserEntity } from './entities/user.entity';
+import { CreateUSerDto } from './interface/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post()
+  signupUser(@Body() body: CreateUSerDto): Promise<UserEntity> {
+    return this.userService.createUser(body);
+  }
+}
